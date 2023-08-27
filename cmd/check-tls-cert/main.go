0ecb9beb953dab6ce40f2b361d21eaacb420eb77
Nicolai Mollerup <nicolai@mollerup.me>
2023-08-27 23:30:12 +0200

    check-tls-cert created

---
+++ cmd/check-tls-cert/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-plugin-sdk/sensu"
+)
+
+// Config represents the check plugin config.
+type Config struct {
+	sensu.PluginConfig
+	Host               string
+	TrustedCAFile      string
+	InsecureSkipVerify bool
+	Port               int
+	Timeout            int
+	Warning            int
+	Critical           int
+}
+
+var (
+	tlsConfig tls.Config
+
+	plugin = Config{
+		PluginConfig: sensu.PluginConfig{
+			Name:     "check-tls-cert",
+			Short:    "TLS expiry check",
+			Keyspace: "sensu.io/plugins/http-check/config",
+		},
+	}
+
+	options = []sensu.ConfigOption{
+		&sensu.PluginConfigOption[string]{
+			Path:     "hostname",
+			Argument: "hostname",
+			Default:  "http://localhost:80/",
+			Usage:    "hostname to check",
+			Value:    &plugin.Host,
+		},
+		&sensu.PluginConfigOption[bool]{
+			Path:      "insecure-skip-verify",
+			Argument:  "insecure-skip-verify",
+			Shorthand: "i",
+			Default:   false,
+			Usage:     "Skip TLS certificate verification (not recommended!)",
+			Value:     &plugin.InsecureSkipVerify,
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:      "trusted-ca-file",
+			Argument:  "trusted-ca-file",
+			Shorthand: "t",
+			Default:   "",
+			Usage:     "TLS CA certificate bundle in PEM format",
+			Value:     &plugin.TrustedCAFile,
+		},
+		&sensu.PluginConfigOption[int]{
+			Path:      "",
+			Argument:  "warning",
+			Shorthand: "w",
+			Usage:     "Number of days left",
+			Value:     &plugin.Warning,
+		},
+		&sensu.PluginConfigOption[int]{
+			Path:      "",
+			Argument:  "critical",
+			Shorthand: "c",
+			Usage:     "Number of days left",
+			Value:     &plugin.Critical,
+		},
+		&sensu.PluginConfigOption[int]{
+			Path:      "",
+			Argument:  "port",
+			Shorthand: "p",
+			Default:   443,
+			Usage:     "TCP port to connect to, default 443",
+			Value:     &plugin.Port,
+		},
+	}
+)
+var validate *validator.Validate
+
+func main() {
+	validate = validator.New()
+
+	check := sensu.NewCheck(&plugin.PluginConfig, options, checkArgs, executeCheck, false)
+	check.Execute()
+}
+
+func checkArgs(event *corev2.Event) (int, error) {
+	if len(plugin.Host) == 0 {
+		return sensu.CheckStateWarning, fmt.Errorf("--hostname is required")
+	}
+	err := validate.Var(plugin.Host, "fqdn")
+	if err != nil {
+		return sensu.CheckStateWarning, fmt.Errorf("hostname is not a valid FQDN")
+	}
+	if plugin.Critical <= 0 {
+		return sensu.CheckStateWarning, fmt.Errorf("--critical is required")
+	}
+	if plugin.Warning <= 0 {
+		return sensu.CheckStateWarning, fmt.Errorf("--warning is required")
+	}
+	if plugin.Warning <= plugin.Critical {
+		return sensu.CheckStateWarning, fmt.Errorf("warning cannot be lower than Critical value")
+	}
+	if len(plugin.TrustedCAFile) > 0 {
+		caCertPool, err := corev2.LoadCACerts(plugin.TrustedCAFile)
+		if err != nil {
+			return sensu.CheckStateWarning, fmt.Errorf("error loading specified CA file")
+		}
+		tlsConfig.RootCAs = caCertPool
+	}
+	tlsConfig.InsecureSkipVerify = plugin.InsecureSkipVerify
+
+	return sensu.CheckStateOK, nil
+}
+func executeCheck(event *corev2.Event) (int, error) {
+	fqdn := plugin.Host + ":" + fmt.Sprint(plugin.Port)
+	conn, err := tls.Dial("tcp", fqdn, nil)
+	if err != nil {
+		return sensu.CheckStateCritical, fmt.Errorf("%v", err)
+	}
+	defer conn.Close()
+
+	timeNow := time.Now()
+
+	cert := conn.ConnectionState().PeerCertificates[0]
+
+	// Get expiry time in hours
+	expiresInHours := int64(cert.NotAfter.Sub(timeNow).Hours())
+	expiresInDays := int(expiresInHours / 24)
+	// Check the expiration.
+	if timeNow.AddDate(0, 0, plugin.Warning).After(cert.NotAfter) {
+		fmt.Printf("warning: cert expires in %v days", expiresInDays)
+		return sensu.CheckStateWarning, nil
+	}
+	if timeNow.AddDate(0, 0, plugin.Critical).After(cert.NotAfter) {
+		fmt.Printf("critical: cert expires in %v days", expiresInDays)
+		return sensu.CheckStateCritical, nil
+	}
+	fmt.Printf("certificate for %v:%v expires in %v days\n", plugin.Host, plugin.Port, expiresInDays)
+	return sensu.CheckStateOK, nil
+}
---
+++ go.mod
@@ -0,0 +1,59 @@
+module github.com/nmollerup/sensu-check-tls
+
+go 1.20
+
+require (
+	github.com/go-playground/validator/v10 v10.14.1
+	github.com/sensu/sensu-go/api/core/v2 v2.14.0
+	github.com/sensu/sensu-plugin-sdk v0.16.0
+	github.com/stretchr/testify v1.8.2
+
+)
+
+require (
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/echlebek/timeproxy v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/robertkrimen/otto v0.0.0-20221006114523-201ab5b34f52 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/sensu/sensu-go/types v0.10.0 // indirect
+	github.com/sensu/sensu-licensing v0.1.2 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.5 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/ini.v1 v1.51.0 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
